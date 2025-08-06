fff5e320f51e2a4c0b030302432f1050012c35ac
Tim Gafarov <timu.ryan@yandex.ru>
2025-08-07 00:40:28 +0300

    feat: add the Sprite class for drawing images

---
+++ src/Vector2.js
@@ -0,0 +1,7 @@
+
+export class Vector2 {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,46 @@
 import { resources } from './Resource.js';
+import { Sprite } from './Sprite.js';
+import { Vector2 } from './Vector2.js';
 
 const canvas = document.querySelector('#game-canvas');
 const ctx = canvas.getContext("2d");
 
-const draw = () => {  
-  const sky = resources.images.sky;
-  if (sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0)
-  }
+const skySprite = new Sprite({
+  resource: resources.images.sky,
+  frameSize: new Vector2(320, 180),
+})
 
-  const ground = resources.images.ground;
-  if (ground.isLoaded) {
-    ctx.drawImage(ground.image, 0, 0)
-  }
+const groundSprite = new Sprite({
+  resource: resources.images.ground,
+  frameSize: new Vector2(320, 180),
+})
 
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1
+})
+
+const heroPos = new Vector2(16 * 6, 16 * 5)
+
+const shadow = new Sprite({
+  resource: resources.images.shadow,
+  frameSize: new Vector2(32, 32),
+})
+
+const draw = () => {
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+
+  // center the Hero in the cell
+  const heroOffset = new Vector2(-8, -21);
+  const heroPosX = heroPos.x + heroOffset.x;
+  const heroPosY = heroPos.y + 1 + heroOffset.y;
+
+  shadow.drawImage(ctx, heroPosX, heroPosY)
+  hero.drawImage(ctx, heroPosX, heroPosY)
 }
 
 setInterval(() => {
